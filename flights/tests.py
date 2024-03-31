2a5887c7f55f4fe0bebf8ba7f844d087e01fda90
birlayugal <birla.yugal2014@gmail.com>
2024-03-31 20:17:36 +0530

    included back invalid flight page

--- flights/tests.py
+++ flights/tests.py
@@ -58,6 +58,12 @@ class FlightTestCase(TestCase):
 
 
     
+    def test_invalid_flight_page(self):
+        max_id = Flight.objects.all().aggregate(Max("id"))["id__max"]
+
+        c = Client()
+        response = c.get(f"/flights/{max_id + 1}")
+        self.assertEqual(response.status_code, 404)
 
 
     def test_flight_page_passengers(self):
